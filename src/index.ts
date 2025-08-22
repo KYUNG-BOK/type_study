a65db5482ac97d0150d72e3b0c8f4aad13455822
kyeongbok <kyeongbok_0627@kakao.com>
2025-08-22 10:45:55 +0900

    과제 끄읏

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+// string
+let userName: string = "코코";
+console.log("이름:", userName);
+
+// number
+let age: number = 35;
+console.log("나이:", age);
+
+// boolean
+let isStudent: boolean = false;
+console.log("학생 여부:", isStudent);
+
+// null
+let emptyValue: null = null;
+console.log("null 값:", emptyValue);
+
+// any
+let randomValue: any = "hello";
+randomValue = 123;
+console.log("anytype:", randomValue);
+
+// any값 변경
+randomValue = 123;
+console.log("any (number):", randomValue);
+
+// any값 변경2
+randomValue = true;
+console.log("any (boolean):", randomValue);
+
